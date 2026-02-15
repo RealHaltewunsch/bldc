a320013492db214f48a0eacfbc94134cf31b9753
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2026-02-15 15:11:01 +0100

    Update hw_mini_pi.h
    Select Mini Pi V1 as standard

--- hwconf/Haltewunsch/mini-pi/hw_mini_pi.h
+++ hwconf/Haltewunsch/mini-pi/hw_mini_pi.h
@@ -1,6 +1,6 @@
 /*
 	Haltewunsch Mini Pi hardware config.
-	Default revision is V0. Define HW_MINI_PI_IS_V1 to build V1.
+	Default revision is V1. Define HW_MINI_PI_IS_V0 to build V0.
 */
 
 #ifndef HW_MINI_PI_H_
@@ -8,10 +8,10 @@
 
 #define HW_MINI_PI
 
-#if defined(HW_MINI_PI_IS_V1)
-#define HW_HALTEWUNSCH_MINI_PI_V1
-#else
+#if defined(HW_MINI_PI_IS_V0)
 #define HW_HALTEWUNSCH_MINI_PI_V0
+#else
+#define HW_HALTEWUNSCH_MINI_PI_V1
 #endif
 
 #include "hw_mini_pi_core.h"
