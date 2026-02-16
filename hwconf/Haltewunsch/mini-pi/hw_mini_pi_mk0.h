5f3815cea0de6bf6da8d96eac4df1a6cde9a803c
RealHaltewunsch <julius.merz@web.de>
2026-02-16 21:25:14 +0100

    Mini Pi added

---
+++ hwconf/Haltewunsch/mini-pi/hw_mini_pi_mk0.h
@@ -0,0 +1,13 @@
+/*
+	Haltewunsch Mini Pi hardware config (MK0).
+*/
+
+#ifndef HW_MINI_PI_MK0_H_
+#define HW_MINI_PI_MK0_H_
+
+#define HW_MINI_PI
+#define HW_HALTEWUNSCH_MINI_PI_MK0
+
+#include "hw_mini_pi_core.h"
+
+#endif /* HW_MINI_PI_MK0_H_ */
---
+++ hwconf/Haltewunsch/mini-pi/hw_mini_pi_mk1_no_limits.h
@@ -0,0 +1,14 @@
+/*
+	Haltewunsch Mini Pi hardware config (MK1, no limits).
+*/
+
+#ifndef HW_MINI_PI_MK1_NO_LIMITS_H_
+#define HW_MINI_PI_MK1_NO_LIMITS_H_
+
+#define HW_MINI_PI
+#define HW_HALTEWUNSCH_MINI_PI_MK1
+#define DISABLE_HW_LIMITS
+
+#include "hw_mini_pi_core.h"
+
+#endif /* HW_MINI_PI_MK1_NO_LIMITS_H_ */
